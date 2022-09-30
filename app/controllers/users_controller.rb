6cd3ee8de7cdcb8ed6c1df7e1f5da79cdc9a0e80
Alexa Macias <alexamacias192@gmail.com>
2022-09-30 09:41:28 -0400

    Error handling and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
   end
 
   def create
+    debugger
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render 'new'
     end
