7c26c2ce3bb0ac9dfc853dd588fc5613bf25918a
Alexa Macias <alexamacias192@gmail.com>
2022-11-15 17:04:18 -0500

    Logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
-class SessionsController < ApplicationController
-  def new
+class SessionsControler < ApplicationController
+
+def new
+end
+
+def create
+  user = User.find_by(email: params[:session][:email].downcast)
+  if user && user.authentication(params[:session][:password])
+  else
+    render 'new'
   end
+end
+
+def destroy
 end
