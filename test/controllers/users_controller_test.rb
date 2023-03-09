6e54eaaba64ac063312165f97c9ef90f3eb38fb8
Alexa M <88990394+a-maci29@users.noreply.github.com>
2023-03-09 14:20:23 -0500

    10.3.5 Partial Refactoring

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -32,7 +32,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
-    get edit_user_path(@user)
+    get user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
