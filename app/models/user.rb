931060f348f06150d27e84400ebc92525f455f3d
Alexa M <88990394+a-maci29@users.noreply.github.com>
2023-04-19 17:04:02 -0400

    Something broke.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -33,9 +33,9 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
-end
 
 private
+
   def downcase_email
     self.email = email.downcase
   end
@@ -44,3 +44,4 @@ private
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+end
